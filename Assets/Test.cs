4600c4142dc2484397957d0595d0a512f0536742
水田明良 <kiriboshi31@gmail.com>
2020-05-28 14:02:51 +0900

    2回目のコミット。
    基本課題のスクリプトの訂正と発展課題のスクリプトの追加。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,25 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+	private int hp = 100;          // 体力
+	private int power = 25; // 攻撃力
+	private int mp = 53;
+	private int makou = 5; //消費MPの値
+
+	// 攻撃用の関数
+	public void Attack()
+	{
+		Debug.Log(this.power + "のダメージを与えた");
+	}
+
+	// 防御用の関数
+	public void Defence(int damage)
+	{
+		Debug.Log(damage + "のダメージを受けた");
+		// 残りhpを減らす
+		this.hp -= damage;
+	}
+
+	//魔法用の関数
+	public void Magic()
+	{
+		if (mp >= makou)
+		{
+			mp = mp - makou;
+			Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+		}
+		else
+		{
+			Debug.Log("MPが足りないため魔法が使えない。");
+		}
+	}
+}
+
 public class Test : MonoBehaviour {
+
+
 
 	// Use this for initialization
 	void Start () {
-		int[] array = new int[5];
+		int[] array = { 10, 20, 30, 40, 50 };
 
-		array[0] = 10;
-		array[1] = 20;
-		array[2] = 30;
-		array[3] = 40;
-		array[4] = 50;
-
-		for(int i = 0; i < 5; i++)
+		for(int i = 0; i < array.Length; i++)
 		{
 			Debug.Log(array[i]);
 		}
-		for (int i = 4; i >-1; i--)
+		for (int i = array.Length-1; i >-1; i--)
 		{
 			Debug.Log(array[i]);
+		}
+
+		Boss lastboss = new Boss();
+
+		for (int i = 1; i <= 11; i++)
+		{
+			lastboss.Magic();
 		}
 	}
 	
